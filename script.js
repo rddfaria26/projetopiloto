b96d06a84eb5800f54a388654afd4fee8deb7f1a
rddfaria26 <fariarodrigo.oliveira93@gmail.com>
2026-06-03 14:52:22 -0300

    -m ":tada: initial commit"

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/pppp.png")
+  } else {
+    img.setAttribute("src", "./assets/rddfaria26.png")
+  }
+}
